ecf7ab17d137c21a7e1a03120c602fb58886b957
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-11-17 08:38:11 +0300

    added alert on item

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,10 +1,41 @@
-
+import useAuth from "../../Hooks/useAuth";
+import Swal from 'sweetalert2'
+import { useNavigate } from "react-router-dom";
 const FoodCard = ({ item }) => {
-    console.log(item)
+    ///custom hook
+    const { user } = useAuth
+
     const { category, image, name, price, recipe } = item
 
+    const navigate = useNavigate()
     const handleAddToCart = food => {
-        console.log(food)
+        ///Enforce user to login before add to the cart
+        ///check user login or not and if login cart can be added
+        if (user && user.email) {
+            fetch(`http://localhost:5000/cart`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(food)
+            })
+        }
+        else {
+            Swal.fire({
+                title: "You are not logged In",
+                text: "Please login to add to the cart?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    ///send the user to the login page
+                    navigate('/login')
+                }
+            });
+        }
     }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
